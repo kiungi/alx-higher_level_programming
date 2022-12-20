52bcc9b99f6d1d72c728ce9ccc0b5bdf7127ebb7
joakim <kiungi@mail.com>
2022-12-20 10:51:53 -0800

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+"""Square generation module for Python project 0x06
+"""
 
+
+class Square:
+    """class defined for square generation
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
         self.__size = size
